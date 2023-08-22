8750dfd43555e4968f35bb4e77968dfd3a622084
Wiltort <wiltort21@gmail.com>
2023-08-22 20:30:38 +0500

    api/v1/posts

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from posts.models import Post, Comment
+
+from posts.models import Post
 from .serializers import PostSerializer
+
+
 # Create your views here.
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
